a3bcd94f91b44dde8ee166e21a10ed50f2eb14e2
witherr <sdssouvic99@gmail.com>
2023-12-02 13:39:34 +0530

    Explain the 2023 D1P1 logic

--- 2023/day_1/part_2/main.py
+++ 2023/day_1/part_2/main.py
@@ -1,10 +1,14 @@
 with open("2023/day_1/part_2/input.txt", "r") as f:
     inp = f.read()
     for digit, word in enumerate(["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]):
+        # Replace the spelled out digit for every digit
+        # with: the spelled out digit with the digit in the middle
+        # e.g. eightwothree -> ei8ght2woth3ree
+        # so that overlapping ones still work for both digits
         inp = inp.replace(word, f"{word[:len(word)//2]}{digit}{word[len(word)//2:]}")
     lines = inp.split("\n")
 
-# i am mentally deranged
+# i dont event wanna explain this
 answer = sum(
     [int(f[0] + f[-1]) for line in lines if (f := [l for l in line if l.isdigit()])]
 )
