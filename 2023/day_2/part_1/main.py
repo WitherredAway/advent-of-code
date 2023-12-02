21793690966c06b34d8e7cf9f5d6b684c73d9f6b
witherr <sdssouvic99@gmail.com>
2023-12-02 15:41:45 +0530

    2023 D2P1: Solve puzzle

--- 2023/day_2/part_1/main.py
+++ 2023/day_2/part_1/main.py
@@ -1,27 +1,37 @@
+import itertools
+
+
 with open("2023/day_2/part_1/input.txt", "r") as f:
     lines = f.readlines()
 
+limits = {"red": 12, "green": 13, "blue": 14}
+
+
+def key(num_colour):
+    num_colour[1]  # Return the colour
+
+
 games = {
-    int(game.split()[-1]): [
-        {
-            colour: int(num)
-            for num, colour in [num_colour.split() for num_colour in _set.split(", ")]
-        }
-        for _set in cubes.split("; ")
-    ]
+    int(game.split()[-1]): {
+        colour: max([int(n) for n, c in group])
+        for colour, group in itertools.groupby(
+            sorted(
+                [
+                    num_colour.split()
+                    for num_colour in cubes.replace("; ", ", ").split(", ")
+                ],
+                key=key,
+            ),
+            key=key,
+        )
+    }
     for game, cubes in [line.replace("\n", "").split(": ") for line in lines]
 }
 
-limits = {"red": 12, "green": 13, "blue": 14}
-
 possible = [
     game_id
-    for game_id, cubes in games.items()
-    if all(
-        [
-            max([_set.get(colour, 0) for _set in cubes]) < limit
-            for colour, limit in limits.items()
-        ]
-    )
+    for game_id, max_cubes in games.items()
+    if all([max_cubes.get(colour, 0) <= limit for colour, limit in limits.items()])
 ]
-print(sum(possible))
+
+print(f"The sum of the IDs of the games with the specified limits is: {sum(possible)}")
