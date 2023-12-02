bb62d7288b73bd50ee4fb1ba593fa89ec2625b1f
witherr <sdssouvic99@gmail.com>
2023-12-02 13:47:17 +0530

    Fix typo

--- 2023/day_1/part_2/main.py
+++ 2023/day_1/part_2/main.py
@@ -8,7 +8,7 @@ with open("2023/day_1/part_2/input.txt", "r") as f:
         inp = inp.replace(word, f"{word[:len(word)//2]}{digit}{word[len(word)//2:]}")
     lines = inp.split("\n")
 
-# i dont event wanna explain this
+# i dont even wanna explain this
 answer = sum(
     [int(f[0] + f[-1]) for line in lines if (f := [l for l in line if l.isdigit()])]
 )
